ca217ea2e3ca7718d5b3cbbf0df57858183993da
jhozasa000 <jhozasa@hotmail.com>
2023-10-09 00:54:50 -0500

    se quitan comentarios

--- routes/login.js
+++ routes/login.js
@@ -5,12 +5,8 @@ const pool = require('../database/dbmongo')
   
 // Handling request using router 
 router.post("/",async(req,res,next)=>{ 
-   // client.db().collection('login').insertOne({ user: 'admin', pass: '123', level: 1 , state: 1 });
-
-   // const usuarios = await pool.db().collection('login').find().toArray();
     const data = req.body
     const usuarios = await pool.db().collection('login').findOne(data);
-
     if(!usuarios){
         const info =  {
             error:'Validar datos ingresados'
